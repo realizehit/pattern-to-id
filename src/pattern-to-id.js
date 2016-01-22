c5e7c03258977c69bb244b371e36cdd821c5dbf2
José Moreira <jose.moreira@findhit.com>
2016-01-22 19:08:44 +0000

    Add files

---
+++ src/pattern-to-id.js
@@ -0,0 +1,29 @@
+var Subscription = require( 'realizehit-subscription' )
+var crypto = require( 'crypto' )
+
+var debug = require( 'debug' )( 'realizehit:ws:pattern-to-id' )
+
+function pattern2id ( pattern ) {
+
+    debug( "trying to figure out id of", pattern )
+
+    var stringified =
+        typeof pattern == 'object' && (
+            pattern instanceof Subscription && pattern.toString() ||
+            (new Subscription( pattern )).toString()
+        ) ||
+        typeof pattern == 'string' && pattern ||
+        false
+
+    if ( ! stringified ) {
+        return
+    }
+
+    return crypto
+        .createHash( 'md5' )
+        .update( stringified )
+        .digest( 'hex' )
+        .substr( 0, 8 )
+}
+
+module.exports = pattern2id
